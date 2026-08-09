fd38da96610cf71d20aef899f4c65a370b3f5c61
Pedro Anuda <pedroanuda@gmail.com>
2026-08-09 19:21:41 -0300

    ♻️ refactor: improve `vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,19 @@ import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
+const host = process.env.TAURI_DEV_HOST
+
 export default defineConfig({
   plugins: [react(), svgr(), tsconfigPaths(), tailwindcss()],
   server: {
-    host: "localhost",
+    host: host || "localhost",
     port: 3000,
+    strictPort: true,
+    hmr: host ? {
+      protocol: 'ws',
+      host,
+      port: 3001,
+    } : undefined
   },
   base: "/",
   build: {
