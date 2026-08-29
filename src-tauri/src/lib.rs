c54bfb68604a6f52e45e76ad0fda439a8ffdca1a
Pedro Anuda <pedroanuda@gmail.com>
2026-08-29 13:40:58 -0300

    ✨ feat: prevent desktop action buttons from showing in mobile

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -14,6 +14,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_os::init())
         .setup(|app| {
             infrastructure::setup(app)?;
             Ok(())
--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -1,5 +1,6 @@
 import { getCurrentWindow } from "@tauri-apps/api/window";
 import { getName } from "@tauri-apps/api/app";
+import { type } from "@tauri-apps/plugin-os";
 import { MinimizeIcon, MaximizeIcon, CloseIcon, ToggleMaximizeIcon } from "common/icons";
 import { SvgIcon } from "@mui/material";
 import { createPortal } from "react-dom";
@@ -7,6 +8,8 @@ import styles from "./AppBar.module.css";
 import React from "react";
 const appName = await getName();
 const appWindow = getCurrentWindow();
+
+const osType = type();
 
 export default function AppBar() {
     const [maximizeIcon, setMaximizeIcon] = React.useState<typeof MaximizeIcon>(MaximizeIcon);
@@ -18,21 +21,23 @@ export default function AppBar() {
         <div className={styles.titlebarDecoration} data-tauri-drag-region>
             {appName}
         </div>, document.body)}
-        <div>
-            <div className={styles.titlebarButton} onClick={() => appWindow.minimize()}>
-                <MinimizeIcon />
+        {!['ios', 'android'].includes(osType) && (
+            <div>
+                <div className={styles.titlebarButton} onClick={() => appWindow.minimize()}>
+                    <MinimizeIcon />
+                </div>
+                <div id="maximized" className={styles.titlebarButton} onClick={() => {
+                    appWindow.toggleMaximize();
+                    appWindow.isMaximized()
+                    .then(res => res ? setMaximizeIcon(MaximizeIcon) : setMaximizeIcon(ToggleMaximizeIcon))}}>
+                        <SvgIcon component={maximizeIcon} />
+                </div>
+                <div className={`${styles.titlebarButton} ${styles.closeButton}`} 
+                onClick={() => appWindow.close()}>
+                    <CloseIcon />
+                </div>
             </div>
-            <div id="maximized" className={styles.titlebarButton} onClick={() => {
-                appWindow.toggleMaximize();
-                appWindow.isMaximized()
-                .then(res => res ? setMaximizeIcon(MaximizeIcon) : setMaximizeIcon(ToggleMaximizeIcon))}}>
-                    <SvgIcon component={maximizeIcon} />
-            </div>
-            <div className={`${styles.titlebarButton} ${styles.closeButton}`} 
-            onClick={() => appWindow.close()}>
-                <CloseIcon />
-            </div>
-        </div>
+        )}
     </div>
     </>
     )
